0ee9aceabe9c4ab667df2d4950fe81ca0d110fe9
olZabi <olzabi@hotmail.com>
2020-12-07 22:03:03 +0200

    upd

--- lab9_v9/Class/Commodity.cs
+++ lab9_v9/Class/Commodity.cs
@@ -1,27 +1,30 @@
 ﻿using System;
+using System.Collections;
 using System.Drawing;
 using System.Threading;
 using lab9_v9.Interfaces;
 
 namespace lab9_v9.Class
 {
-    public abstract class Commodity: ICommodity
+    public abstract class Commodity : ICommodity
     {
         // Fields
         private static int _nextId;
-        public int InternalId { get;  set; }
+        public int InternalId { get; set; }
 
         public string Name { get; set; }
         public string Brand { get; set; }
         public Color Color { get; set; }
-        public string Size { get; set; }
+        public Int32 Size { get; set; }
         public string Style { get; set; }
+        public Int32 Cost { get; set; }
         public bool IsInStoke { get; set; }
-        
-        
+
+
         // Совсем не принципиально будет ли объект иммутабельным или нет, поэтому используем две формы.
-        protected Commodity() {}
-        protected Commodity(string name, string brand, Color color, string size, string style, bool isInStoke)
+        protected Commodity() { }
+        protected Commodity(string name, Int32 cost, string brand, Color color, Int32 size, string style,
+            bool isInStoke)
         {
             InternalId = Interlocked.Increment(ref _nextId);
             Brand = brand;
@@ -30,5 +33,35 @@ namespace lab9_v9.Class
             Style = style;
             IsInStoke = isInStoke;
         }
+
+        // Methods 
+        // IComparable --> CompareTo, not done yet
+        public virtual int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+            Commodity c = obj as Commodity; // double casting 
+            if (c == null)
+            {
+                throw new Exception("error: CompareTo()");
+            }
+            
+            return CompareTo(c);
+        }
+        
+        // IComparer --> Compare √  
+        public virtual int Compare(object obj1, object obj2)
+        {
+            return ((new CaseInsensitiveComparer()).Compare(obj1, obj2));
+        }
+        
+        // IEnumerable --> GetEnumerator
+
+        // public virtual IEnumerator GetEnumerator()
+        // {
+        //     
+        // }
     }
 }
--- lab9_v9/Interfaces/Commodity.cs
+++ lab9_v9/Interfaces/Commodity.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Drawing;
 
 namespace lab9_v9.Interfaces
 {
-    public interface ICommodity
-    {
+    public interface ICommodity : IComparable, IComparer, IEnumerable
+    { 
+        // Fields
         int InternalId { get; set; }
-        string Name { get; set; }
         string Brand { get; set; }
         Color Color { get; set; }
-        string Size { get; set; }
+        Int32 Size { get; set; }
+        Int32 Cost { get; set; }
         string Style { get; set; }
         bool IsInStoke { get; set; }
+
+        string Name { get; set; }
+
+        // Methods
+        int IComparable.CompareTo(object obj);
+        int IComparer.Compare(object obj1, object obj2);
+        IEnumerator IEnumerable.GetEnumerator();
     }
 }
