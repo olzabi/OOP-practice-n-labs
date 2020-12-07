60452aa664b11fcc6a8b1970a4f64a5ceeb08d07
olZabi <olzabi@hotmail.com>
2020-12-07 20:52:33 +0200

    lab9_v9

---
+++ lab9_v9/Interfaces/Commodity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+
+namespace lab9_v9.Interfaces
+{
+    public interface ICommodity
+    {
+        int InternalId { get; set; }
+        string Name { get; set; }
+        string Brand { get; set; }
+        Color Color { get; set; }
+        string Size { get; set; }
+        string Style { get; set; }
+        bool IsInStoke { get; set; }
+    }
+}
---
+++ lab9_v9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using lab9_v9.Class;
+
+namespace lab9_v9
+{
+    /*
+     lab 9.1 --> Toy, product, commodity --> done
+     create interface for one of them --> done
+     use 2 abstract methods in inheritance classes --> done
+     
+     lab 9.2 --> 
+     1) create an array of Commodity objects
+     Use interface <IComparable> to compare goods by price in method CompareTo()
+     2) Use IComparer in classes to compare goods by price and size
+     3) Use interface <IEnumerable>. O/ goods list ordered by price
+     */
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
