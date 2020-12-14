701810bd00c6cee926b9a661866fe7f00889c9f2
olZabi <olzabi@hotmail.com>
2020-12-14 17:20:37 +0200

    T2

---
+++ FinalTest_t2/FinalTest_t2/Product.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+
+namespace FinalTest_t2
+{
+    public class Product 
+    {
+        // id field
+        private static int NextId;
+        private int Id { get; set; }
+        
+        
+        // product fields
+        private int ProductCountryCode { get; set; }
+        private string ProductName { get; set; }
+        private DateTime ExpirationDate { get; set; }
+        private int Price { get; set; }
+        private int Quantity { get; set; }
+
+        
+        public int Country => ProductCountryCode;
+        public string Name => ProductName;
+        public DateTime Expiration => ExpirationDate;
+        public int Cost => Price;
+        public int Quant => Quantity;
+        
+        
+        // UPC 
+        private int ManufacturerCode { get; set; }
+        
+        private int ProductCode { get; set; }
+        private string UpcCode => $"{ProductCountryCode}{ManufacturerCode}{ProductCode}" ;
+        public string Upc => UpcCode + Ean13(UpcCode).ToString();
+        
+        
+        // Constructor
+        public Product(string name, int country,DateTime expiration, int price, int quantity, int manufacturerCode,
+            int productCode)
+        {
+            Id = Interlocked.Increment(ref NextId);
+
+            ProductName = name;
+            ProductCountryCode = country;
+            ExpirationDate = expiration;
+            Price = price;
+            Quantity = quantity;
+            
+            ProductCode = productCode;
+            ManufacturerCode = manufacturerCode;
+        }
+
+        
+        // Methods    
+        static int Ean13(string barcode) 
+        {
+            int counter = 0;
+            int sum = 0;
+            
+            
+            if (barcode.Length != 12 & barcode.Length != 8)
+                throw new Exception("Bad barcode length");
+
+            for (int j = barcode.Length - 2; j > -1; j--)
+            {
+                counter += 1;
+                if (counter % 2 == 0) {
+                    sum += int.Parse(barcode[j].ToString());
+                } else {
+                    sum += int.Parse(barcode[j].ToString()) * 3;
+                }
+            }
+            sum = (10 - (sum % 10)) % 10;
+            return sum;
+        }
+    }
+}
---
+++ FinalTest_t2/FinalTest_t2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FinalTest_t2
+{
+    /*
+     9. Product:
+        id,                
+        Найменування,      
+        UPC,               
+        Виробник,          
+        Ціна,              
+        Термін зберігання, 
+        Кількість.         
+        
+        Output:
+        a) список товарів для заданого найменування;                                   
+        b) список товарів для заданого найменування, ціна яких становить менше задану; 
+        c) список товарів, термін зберігання яких більше заданого                      
+        
+        
+        
+        ean13 algorithm
+        1. sum up even, sum up odd 
+        ex. 
+        **************************************
+        | 13 12 11 10 9 8 7 6 5 4 3 2 1      | <--- digit position
+        |                                    |
+        | 3  0  5  5  6 4 0 0 7 1 5 3        | <--- 3d - country, 4d - manuf., 5d product code, 1d -> K
+        | 3  +  5  +  6 + 0 + 7 + 5 = 26     | <--- odds
+        |    0  +  5  + 4 + 0 + 1 + 3 = 13   | <--- even
+        **************************************
+        2. even * 3 + odd = res
+        3. res %= 10 
+        4. (10 - res) % 10 = K (1st position)
+     */
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+
+            Product p1 = new Product("Furniture", 383, new DateTime(2020, 10, 21), 450, 5, 3442,
+                51519);
+            
+            Product p2 = new Product("Furniture", 401, new DateTime(2020, 12, 1), 760, 5, 8993,
+                    40478);
+            
+            Product p3 = new Product("Accessory", 600, new DateTime(2021, 3, 21), 531, 5, 4114,
+                92441);
+
+            products.Add(p1);
+            products.Add(p2);
+            products.Add(p3);
+
+            //
+            var a = products.Where(p => p.Name == "Furniture").OrderBy(p => p.Name);
+            var b = products.Where(p => p.Name == "Accessory").OrderBy(p => p.Cost < 400);
+            var c = products.Where(p => p.Expiration.Date < new DateTime(2020, 11, 20)).OrderBy(p => p.Expiration);
+            
+            Console.WriteLine("OUTPUT A");
+            foreach (var p in a)
+            {
+                Console.WriteLine("Name: {0} \nPrice: {1}, Expiration: {2}, UPC: {3}",
+                    p.Name, p.Cost, p.Expiration.ToShortDateString(), p.Upc);
+            }
+            
+            Console.WriteLine("OUTPUT B");
+            foreach (var p in b)
+            {
+                Console.WriteLine("Name: {0} \nPrice: {1}, Expiration: {2}, UPC: {3}",
+                    p.Name, p.Cost, p.Expiration.ToShortDateString(), p.Upc);
+            }
+            
+            Console.WriteLine("OUTPUT C");
+            foreach (var p in c)
+            {
+                Console.WriteLine("Name: {0} \nPrice: {1}, Expiration: {2}, UPC: {3}", 
+                    p.Name, p.Cost, p.Expiration.ToShortDateString(), p.Upc);
+            }
+        }
+    }
+}
